c6b57f25eaff5d349fb3a4c404e3802513526433
Javel Queiroz Freitas <javelqueiroz@alu.ufc.br>
2022-04-17 15:18:12 -0300

    dep: add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1>Hello Next</h1>
+    <h1 className={styles.title}>
+      Hello <span>Next</span>
+    </h1>
   )
 }
