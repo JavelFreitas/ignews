ef23081ece2648378a8d323895d3be2be08eafcd
javel freitas <javel.freitas@lead.fortaleza>
2022-05-18 12:40:25 -0300

    chore: update prettierrc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
-import styles from "../styles/home.module.scss";
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
+  let props = { a: 'a', b: 'b' };
+  console.log(`valor de a é ${props.a} e valor de b é ${props.b}`);
+
   return (
     <>
       <h1 className={styles.title}>
         Hello <span>Next.JS</span>
       </h1>
-      {console.log("hello")}
     </>
   );
 }
