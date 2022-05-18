56303f4c3037720172f7c8b9705b53431b3f5fdf
javel freitas <javel.freitas@lead.fortaleza>
2022-05-18 11:06:38 -0300

    test: prettier and list-staged using husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import styles from '../styles/home.module.scss'
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
     <h1 className={styles.title}>
-      Hello <span>Next</span>
+      Hello <span>Next.JS</span>
     </h1>
-  )
+  );
 }
